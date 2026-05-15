9a039258fe96bde3bd17104cf4f934bfb60ed662
Dániel Pápa <danielpapa1166@gmail.com>
2026-05-15 13:43:30 +0200

    refactor in boot mode type handling

--- meta-bkk-setup/recipes-setup/application-manager/files/src/am_boot_mode.h
+++ meta-bkk-setup/recipes-setup/application-manager/files/src/am_boot_mode.h
@@ -1,14 +1,8 @@
 #ifndef AM_BOOT_MODE_H
 #define AM_BOOT_MODE_H
 
-typedef enum {
-  BOOT_MODE_UNDEFINED,
-  BOOT_MODE_WIFI_CONFIG, 
-  BOOT_MODE_API_CONFIG, 
-  BOOT_MODE_NORMAL
-} boot_mode_t;
+#include "am_types.h"
 
-const char * boot_mode_to_string(boot_mode_t mode);
 boot_mode_t determine_boot_mode(void);
 
 #endif // AM_BOOT_MODE_H
--- meta-bkk-setup/recipes-setup/application-manager/files/src/am_types.h
+++ meta-bkk-setup/recipes-setup/application-manager/files/src/am_types.h
@@ -1,6 +1,30 @@
 #ifndef AM_TYPES_H
 #define AM_TYPES_H
 
+typedef enum {
+  BOOT_MODE_UNDEFINED,
+  BOOT_MODE_WIFI_CONFIG, 
+  BOOT_MODE_API_CONFIG, 
+  BOOT_MODE_NORMAL
+} boot_mode_t;
+
+
+static const inline char * boot_mode_to_string(boot_mode_t mode) {
+  switch (mode) {
+    case BOOT_MODE_WIFI_CONFIG:
+      return "WIFI_CONFIG";
+    case BOOT_MODE_API_CONFIG:
+      return "API_CONFIG";
+    case BOOT_MODE_NORMAL:
+      return "NORMAL";
+    case BOOT_MODE_UNDEFINED:
+      return "UNDEFINED";
+    default:
+      return "UNKNOWN_BOOT_MODE";
+  }
+}
+
+
 typedef struct {
   char * name;
   char * binary;
@@ -21,6 +45,7 @@ typedef struct {
 
 typedef enum {
   APP_STATUS_NOT_STARTED,
+  APP_STATUS_NOT_IN_THIS_PHASE,
   APP_STATUS_RUNNING,
   APP_STATUS_EXITED, 
   APP_STATUS_FAILED
