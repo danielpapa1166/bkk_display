db494698f999d54149bc868a58a15e8938ca16ee
Dániel Pápa <danielpapa1166@gmail.com>
2026-05-17 18:40:59 +0200

    folder creation, cli parsing for paths and filenames

--- meta-bkk-setup/recipes-setup/application-manager/files/src/wpa_helper/wpa_config.h
+++ meta-bkk-setup/recipes-setup/application-manager/files/src/wpa_helper/wpa_config.h
@@ -1,6 +1,24 @@
 #ifndef WPA_CONFIG_H
 #define WPA_CONFIG_H
 
+#include <string.h>
+
+// Default paths and filenames 
+#define WPA_CFG_PATH_DEFAULT         "/run"
+#define WPA_CFG_NAME_DEFAULT         "wpa_supplicant-ap.conf"
+#define NETWORK_CFG_PATH_DEFAULT     "/run/systemd/network"
+#define NETWORK_CFG_NAME_DEFAULT     "20-wlan0-ap.network"
+
+typedef struct {
+  const char * wpa_cfg_path;
+  const char * network_cfg_path;
+  const char * wpa_cfg_name;
+  const char * network_cfg_name;
+  const char * wpa_cfg_str;
+  const char * network_cfg_str;
+} wpa_config_t;
+
+
 static const char WPA_CONFIG_AP[] =
     "ctrl_interface=/run/wpa_supplicant\n"
     "update_config=1\n"
@@ -24,5 +42,19 @@ static const char NETWORK_CONFIG_AP[] =
     "[DHCPServer]\n"
     "PoolOffset=10\n"
     "PoolSize=50";
+
+/* NOTE: network_cfg_path default requires /run/systemd to already exist.
+ * This is guaranteed at boot by systemd, but prepare_config_folder only
+ * performs a single-level mkdir. */
+static wpa_config_t wpa_ap_config = {
+  .wpa_cfg_path         = WPA_CFG_PATH_DEFAULT,
+  .network_cfg_path     = NETWORK_CFG_PATH_DEFAULT,
+  .wpa_cfg_name         = WPA_CFG_NAME_DEFAULT,
+  .network_cfg_name     = NETWORK_CFG_NAME_DEFAULT,
+  .wpa_cfg_str          = WPA_CONFIG_AP,
+  .network_cfg_str      = NETWORK_CONFIG_AP
+};
+
+static wpa_config_t wpa_wifi_config; 
 
 #endif /* WPA_CONFIG_H */
--- meta-bkk-setup/recipes-setup/application-manager/files/src/wpa_helper/wpa_helper_main.c
+++ meta-bkk-setup/recipes-setup/application-manager/files/src/wpa_helper/wpa_helper_main.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 #include <string.h>
+#include <sys/stat.h>
+#include <errno.h>
 #include "wpa_config.h"
 
+// ----------------------------------------------------------------------------
+// local types 
+// ----------------------------------------------------------------------------
 
 typedef enum {
   BOOT_MODE_WIFI_CONFIG,
@@ -10,46 +15,136 @@ typedef enum {
   BOOT_MODE_UNKNOWN
 } boot_mode_t;
 
-static boot_mode_t parse_args(int argc, char *argv[]);
+// ----------------------------------------------------------------------------
+// local function prototypes
+// ----------------------------------------------------------------------------
 
+static boot_mode_t parse_args(int argc, char *argv[], wpa_config_t *config);
+static int ensure_dir(const char *path); 
+static int match_key(const char *arg, const char *key, const char **value);
+static int prepare_config_folder(const wpa_config_t * const config);
+static int write_wpa_config(const wpa_config_t * const config);
+
+
+// ----------------------------------------------------------------------------
+// main entry point
+// ----------------------------------------------------------------------------
 
 int main(int argc, char *argv[])
 {
-  const boot_mode_t boot_mode = parse_args(argc, argv);
+  wpa_config_t config = wpa_ap_config;
+  const boot_mode_t boot_mode = parse_args(argc, argv, &config);
 
   if(boot_mode == BOOT_MODE_UNKNOWN) {
     // todo log here
     return -1;
   }
 
-  const char * wpa_cfg_str = NULL;
-  const char * network_cfg_str = NULL;
-  
-  if(boot_mode == BOOT_MODE_WIFI_CONFIG) {
-    wpa_cfg_str = WPA_CONFIG_AP;
-    network_cfg_str = NETWORK_CONFIG_AP;
-  }
+  return 0;
+}
 
+// ----------------------------------------------------------------------------
+// local function implementations
+// ----------------------------------------------------------------------------
+
+/* Returns 1 and sets *value to the argument's value portion if the argument
+   matches "key=<value>", otherwise returns 0. */
+static int match_key(const char *arg, const char *key, const char **value) {
+  const size_t key_len = strlen(key);
+  if (strncmp(arg, key, key_len) == 0 && arg[key_len] == '=') {
+    *value = (const char *)(arg + key_len + 1);
+    return 1;
+  }
   return 0;
 }
 
 
-static boot_mode_t parse_args(int argc, char *argv[]) {
+static boot_mode_t parse_args(int argc, char *argv[], wpa_config_t *config) {
   boot_mode_t boot_mode = BOOT_MODE_UNKNOWN;
 
   if (argc < 2) {
     return boot_mode;
   }
 
-  if(strcmp(argv[1], "boot_mode=wifi_config") == 0) {
+  /* First argument is mandatory: boot_mode=<value> */
+  char *mode_val = NULL;
+  if (!match_key(argv[1], "boot_mode", (const char **)&mode_val)) {
+    return boot_mode;
+  }
+
+  if (strcmp(mode_val, "wifi_config") == 0) {
     boot_mode = BOOT_MODE_WIFI_CONFIG;
-  }
-  else if(strcmp(argv[1], "boot_mode=api_config") == 0) {
+  } else if (strcmp(mode_val, "api_config") == 0) {
     boot_mode = BOOT_MODE_API_CONFIG;
-  }
-  else if (strcmp(argv[1], "boot_mode=normal") == 0) {
+  } else if (strcmp(mode_val, "normal") == 0) {
     boot_mode = BOOT_MODE_NORMAL;
+  } else {
+    return boot_mode;
+  }
+
+  /* Remaining arguments are optional key=value pairs */
+  for (int i = 2; i < argc; i++) {
+    const char *val = NULL;
+    if      (match_key(argv[i], "wpa_cfg_path",     &val)) { 
+      config->wpa_cfg_path     = val; 
+    }
+    else if (match_key(argv[i], "network_cfg_path",  &val)) { 
+      config->network_cfg_path  = val; 
+    }
+    else if (match_key(argv[i], "wpa_cfg_name",      &val)) { 
+      config->wpa_cfg_name      = val; 
+    }
+    else if (match_key(argv[i], "network_cfg_name",  &val)) { 
+      config->network_cfg_name  = val; 
+    }
   }
 
   return boot_mode;
+}
+
+
+static int ensure_dir(const char *path) {
+  if (path == NULL) {
+    return 0;
+  }
+
+  // todo: log error 
+
+
+  struct stat st;
+  if (stat(path, &st) == 0) {
+    if (S_ISDIR(st.st_mode)) {
+      return 0;
+    }
+    fprintf(stderr, "prepare_config_folder: path exists but is not a directory: %s\n", path);
+    return -1;
+  }
+
+  if (errno != ENOENT) {
+    fprintf(stderr, "prepare_config_folder: stat failed for %s: %m\n", path);
+    return -1;
+  }
+
+  if (mkdir(path, 0755) != 0 && errno != EEXIST) {
+    fprintf(stderr, "prepare_config_folder: failed to create directory %s: %m\n", path);
+    return -1;
+  }
+
+  return 0;
+}
+
+
+static int prepare_config_folder(const wpa_config_t * const config) {
+  if (ensure_dir(config->wpa_cfg_path) != 0) {
+    return -1;
+  }
+  if (ensure_dir(config->network_cfg_path) != 0) {
+    return -1;
+  }
+  return 0;
+}
+
+
+static int write_wpa_config(const wpa_config_t * const config) {
+
 }
